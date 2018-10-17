032f90cc623273deffd61ed129543e2d38ee864b
sondos alansi <sondos.alansi@yahoo.com>
2018-10-17 19:38:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showCommits);
-  req.open('GET', 'https://api.github.com/repos/'+user+'/'+ name + '/commits');
+  req.open('GET', 'https://api.github.com/repos/'+ name + '/commits');
   req.send();
 }
 
