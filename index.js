8a54dc7ae90b7cad1195484dd1577aaf8fe9a4a1
sondos alansi <sondos.alansi@yahoo.com>
2018-10-17 18:19:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getRepositories(){
   let user=document.getElementById('user').value;
-  console.log(user);
+  
   const req = new XMLHttpRequest();
-  req.open('GET', 'https://api.github.com/users/:user/repos');
+  req.open('GET', 'https://api.github.com/users/:'+user+'/repos');
   req.send();
 }
