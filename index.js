14035ee94c1efe37e3ccee6e691b15a206f324b4
sondos alansi <sondos.alansi@yahoo.com>
2018-10-17 18:18:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getRepositories(){
   let user=document.getElementById('user').value;
-  
+  console.log(user);
   const req = new XMLHttpRequest();
   req.open('GET', 'https://api.github.com/users/:user/repos');
   req.send();
