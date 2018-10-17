d16048c04307279897f7f138597e3c7db9499d70
sondos alansi <sondos.alansi@yahoo.com>
2018-10-17 19:51:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function showCommits() {
   document.getElementById('details').innerHTML = commitsList;
 }
 function getCommits(el) {
- const user=el.dataset.user;
+ 
   const name = el.dataset.b;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showCommits);
