906dd70119c9dd4591058949338e0e8c3fd20d1b
sondos alansi <sondos.alansi@yahoo.com>
2018-10-17 19:01:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ function showRepositories() {
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
 
 function getRepositories(){
   let user=document.getElementById('user').value;
