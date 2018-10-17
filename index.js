9ff27472d9b9c1b1d539832ba129ccbe68dfb18e
sondos alansi <sondos.alansi@yahoo.com>
2018-10-17 19:53:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,16 +24,15 @@ function showCommits() {
     .join('')}</ul>`;
   document.getElementById('details').innerHTML = commitsList;
 }
-function showCommits() {
+function displayBranches () {
   const commits = JSON.parse(this.responseText);
   const commitsList = `<ul>${commits
     .map(
       commit =>
         '<li><strong>' +
         commit.author.login +
-        '</strong> - ' +
-        commit.commit.message +
-        '</li>'
+        '</strong>  </li>'
+       
     )
     .join('')}</ul>`;
   document.getElementById('details').innerHTML = commitsList;
