4d97821040ff0b460e5ab2e6d6b111c78f1d8890
sondos alansi <sondos.alansi@yahoo.com>
2018-10-17 19:50:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,14 @@ function showCommits() {
 }
 function getCommits(el) {
  const user=el.dataset.user;
+  const name = el.dataset.b;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/'+ name + '/branches');
+  req.send();
+}
+function getCommits(el) {
+ const user=el.dataset.user;
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showCommits);
